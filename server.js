b2216ce77f5ea33365ccd1c3c0147cdee20a5608
kenFlowers <kenneth_flow@Hotmail.com>
2017-10-18 19:37:28 -0500

    put the git ignore file and server.js on

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+app.get("/", function(req, res){
+	res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/view" , function(req, res){
+	res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/", function(req, res){
+	res.sendFile(path.join(__dirname,))
+})
